b25122a93356f72c6bd979b759d752743dd31027
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-02 04:23:04 +0000

    Initialized components directory

---
+++ components/simple_footer_component_1722572584.js
@@ -0,0 +1,30 @@
+/* Summary: This component contains the app's logo and name in a large, bold font. They're both in the same line. And then a line of text underneath it. Both of these lines are center aligned.
+*/
+Vue.component("simple_footer_component_1722572584", {
+    template: `
+    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800">
+            <div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
+                <hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+                <div id="footer-content" class="text-center">
+                    <div class="flex">
+                        <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
+                            <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+                            STARTER_TEMPLATE_APPLICATION_NAME
+                        </a>
+                    </div>
+                </div>
+                <div class="flex">
+                    <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
+                        STARTER_TEMPLATE_FOOTER_CONTENT
+                    </div>
+                </div>
+            </div>
+        </footer> 
+    `,
+    data() {
+        return {
+            expanded: false,
+            tab: null,
+        };
+    },
+});
---
+++ components/simple_hero_component_1722572584.js
@@ -0,0 +1,42 @@
+/* Summary: The hero component contains the following two parts:
+- To the left: There is a hero title text in h1 heading. Underneath it, a hero subtitle line of text. All this text is left-aligned. Underneath this, there are two white buttons in the same line, both justified in alignment.
+- To the right: There is an image relevant to the app.
+*/
+Vue.component("simple_hero_component_1722572584", {
+    template: `
+        <section id="hero-section" class="bg-white dark:bg-gray-900 flex-1">
+            <div id="hero-container" class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+                <div id="hero-text-container" class="mr-auto place-self-center lg:col-span-7">
+                    
+                    <div id="hero-title-container-text" class="flex">
+                        <h1 id="hero-title" class="flex-1 max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">STARTER_TEMPLATE_HERO_PRIMARY_TITLE</h1>
+                    </div>
+
+                    <div id="hero-subtitle-container" class="flex">
+                        <p id="hero-subtitle" class="flex-1 max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">STARTER_TEMPLATE_HERO_SECONDARY_TITLE</p>
+                    </div>
+
+                    <div id="hero-buttons-container" class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                        <div id="hero-button-1-container" class="flex">
+                            <a id="hero-button-1" href="https://github.com/themesberg/landwind" class="flex-1  items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">STARTER_TEMPLATE_HERO_PRIMARY_BUTTON_TEXT</a>
+                        </div>
+
+                        <div id="hero-button-2-container" class="flex">
+                            <a id="hero-button-2" href="https://www.figma.com/community/file/1125744163617429490" class="flex-1  items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">STARTER_TEMPLATE_HERO_SECONDARY_BUTTON_TEXT</a>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div id="hero-image-container" class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                    <img id="hero-image" src="./images/hero.png" alt="hero image">
+                </div>
+            </div>
+        </section>            
+    `,
+    data() {
+        return {
+            expanded: false,
+            tab: null,
+        };
+    },
+});
